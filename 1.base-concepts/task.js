7399885e7ab7bc0597c63befac720e3c00cb60d4
Adyard <eduard.khikmatov@bk.ru>
2022-01-16 21:17:39 +0300

    Added code

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,19 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let x1;
+  let x2;
+  let D = b ** 2 - 4 * a * c;
+  if (D < 0) {
+    arr = [];
+  } else if (d === 0) {
+    x1 = -b / (2 * a);
+    arr = [x1];
+  } else {
+    x1 = (-b + Math.sqrt(D)) / (2 * a);
+    x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr = [x1, x2];
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
