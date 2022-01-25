aeae4ab444a96d8fdb3064d120d6b8973cbd1027
Adyard <eduard.khikmatov@bk.ru>
2022-01-25 13:09:17 +0300

    added 3 tasks

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,55 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
+  let min = Infinity;
+  let max = Infinity;
+  let sum = 0;
+  let avg = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+    avg = sum / arr.length;
+    avg = Number(avg.toFixed(2));
+  }
   return { min: min, max: max, avg: avg };
 }
-
-// Задание 2
+getArrayParams([-99, 99, 10])
+//// Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
-
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  for (var i = 0; i < arrOfArr.length; i++) {
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i])
+    }
+  }
   return max;
 }
-
+arrOfArr = [[1, 2, 3, 4], [10, 20, -10, -20]]
+makeWork(arrOfArr, worker)
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let difference = 0;
+  let min = Infinity;
+  let max = -Infinity;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    difference = Math.abs(max) - Math.abs(min);
+  }
+  return difference;
 }
