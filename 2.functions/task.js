e30fae57d799775131a655eff0bb6b396082ad5a
Adyard <eduard.khikmatov@bk.ru>
2022-01-30 02:32:08 +0300

    added Assignments 3 lectures

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,10 +1,10 @@
 // Задание 1
 function getArrayParams(arr) {
   let min = Infinity;
-  let max = Infinity;
+  let max = -Infinity;
   let sum = 0;
   let avg = 0;
-  for (var i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] > max) {
       max = arr[i];
     }
@@ -12,23 +12,22 @@ function getArrayParams(arr) {
       min = arr[i];
     }
     sum += arr[i];
-    avg = sum / arr.length;
-    avg = Number(avg.toFixed(2));
   }
+  avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
-getArrayParams([-99, 99, 10])
+console.log(getArrayParams([-99, 99, 10]))
 //// Задание 2
-function worker(arr) {
+const w = function worker(arr) {
   let sum = 0;
-  for (var i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
   return sum;
 }
 function makeWork(arrOfArr, func) {
   let max = -Infinity;
-  for (var i = 0; i < arrOfArr.length; i++) {
+  for (let i = 0; i < arrOfArr.length; i++) {
     if (func(arrOfArr[i]) > max) {
       max = func(arrOfArr[i])
     }
@@ -36,20 +35,19 @@ function makeWork(arrOfArr, func) {
   return max;
 }
 arrOfArr = [[1, 2, 3, 4], [10, 20, -10, -20]]
-makeWork(arrOfArr, worker)
+console.log(makeWork(arrOfArr, w))
 // Задание 3
 function worker2(arr) {
-  let difference = 0;
   let min = Infinity;
   let max = -Infinity;
-  for (var i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] > max) {
       max = arr[i];
     }
     if (arr[i] < min) {
       min = arr[i];
     }
-    difference = Math.abs(max) - Math.abs(min);
   }
-  return difference;
+  return Math.abs(max - min);
 }
+console.log(worker2([1, 2, 3]))
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,10 @@
-function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
-}
-
+// Задание 1
+const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
+console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]))
+//// Задание 2
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
+  return resultArr;
 }
+console.log(advancedFilter([-10, -21, 12, 123]));
